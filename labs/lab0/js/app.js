0e8032a2cfa25c2e0aa8085f91c908cff723418b
tj4github2 <102758180+tj4github2@users.noreply.github.com>
2023-01-12 12:54:17 -0500

    adding base lab structure

---
+++ labs/lab0/js/app.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(800, 600);
+    background(248, 18, 82);
+
+    fill(252, 45, 187);
+    rect( 40,50, 100, 20);
+}
+
+function draw() {
+    circle(mouseX, mouseY, 12);
+}
