894fd62952c5fd0d26e466a4a3f29b9c0084318f
tj4github2 <tjmac1@icloud.com>
2023-01-18 15:42:06 -0500

    Changed circles that follow mouse and drawing function

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -4,17 +4,21 @@ function setup() {
 }
 
 function draw() {
-    if (mouseIsPressed) {
-        circle(mouseX, mouseY, 25);
-    }
-}
-
-function draw() {
     fill(213, 24, 24);
     circle(150, 250, 90);
     fill(213, 24, 24);
     circle(410, 250, 90);
     fill(24, 67, 213);
     rect(80, 170, 400, 70);
+
+    stroke(200, 20, 20);
+    fill(200, 220, 20, 90);
+    circle(mouseX, mouseY, 40, 50);
 }
+
+function mouseIsPressed() {
+    background(220, 200, 200)
+}
+
+
 
