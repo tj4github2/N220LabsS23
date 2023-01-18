1e2741c261514fe3b7364288614c8a4f2b30800d
tj4github2 <tjmac1@icloud.com>
2023-01-18 14:58:38 -0500

    Added a mouse command variable

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,15 +1,20 @@
 function setup() {
     createCanvas(800, 600);
+    background(127, 24, 213);
 }
 
 function draw() {
-    background(300);
+    if (mouseIsPressed) {
+        circle(mouseX, mouseY, 25);
+    }
+}
+
+function draw() {
     fill(213, 24, 24);
     circle(150, 250, 90);
     fill(213, 24, 24);
     circle(410, 250, 90);
     fill(24, 67, 213);
     rect(80, 170, 400, 70);
-
-
 }
+
