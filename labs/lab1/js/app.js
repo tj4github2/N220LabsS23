823da2a7300ee78a2181fa8c4b40e9a1723acb69
tj4github2 <tjmac1@icloud.com>
2023-01-18 14:39:44 -0500

    Put code for script, drawing methods, and variables

---
+++ labs/lab1/js/app.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(800, 600);
+}
+
+function draw() {
+    background(300);
+    fill(213, 24, 24);
+    circle(150, 250, 90);
+    fill(213, 24, 24);
+    circle(410, 250, 90);
+    fill(24, 67, 213);
+    rect(80, 170, 400, 70);
+
+
+}
