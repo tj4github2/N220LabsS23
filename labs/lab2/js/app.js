e78fa625b0eaea6794a9ed6eb613a5ee4e60d0bc
tj4github2 <tjmac1@icloud.com>
2023-01-26 14:59:42 -0500

    Updated puckside app.js

--- labs/lab2/js/app.js
+++ labs/lab2/js/app.js
@@ -1,5 +1,17 @@
-var i=0;
+function setup(){
+    createCanvas(400, 300);
+}
+var diameter = 20;
+function draw(){
+    diameter = diameter+1
+    background("yellow");
 
-function draw() {
-    console.log(i++);
+    if(mouseX>200){
+    fill("red");
+    circle(mouseX, mouseY, diameter);
+    }
+    else {
+    fill("blue");
+    circle(mouseX, mouseY, diameter);
+    }
 }
