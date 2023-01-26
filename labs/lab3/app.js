80fcfaf38e3e963d8b0060968ed46166a4305fb5
tj4github2 <tjmac1@icloud.com>
2023-01-26 16:00:12 -0500

    Fixed app.js world wrap

--- labs/lab3/app.js
+++ labs/lab3/app.js
@@ -1,7 +1,29 @@
+//setup is executed once at the launch
+
+// create a global variable for the x position of the circle
 var xPos;
 
-createCanvas(500, 200);
-function setup(){
-    fill(255);
-xPos=0
+function setup() {
+  createCanvas(500, 200);
+  fill(255);
+  // start the variable off at zero
+  xPos = 0; 
+ 
+}
+
+//draw is executed every frame 60 times per second
+function draw() {
+
+	background(244,132,140); 
+
+	xPos += 4;
+
+	// wrap it with a conditional! 
+	if(xPos >= width){
+		// if it is greater than the width of the canvas reset it to 0
+		xPos = 0; 
+	}
+
+	// draw the circle at the given position
+	ellipse(xPos, height/2, 20, 20); 
 }
