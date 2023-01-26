d7064b3eaad6d6c8065c082ec16a6a788b1b50a8
tj4github2 <tjmac1@icloud.com>
2023-01-26 15:56:43 -0500

    Changed world wrap app.js

--- labs/lab3/app.js
+++ labs/lab3/app.js
@@ -1,15 +1,7 @@
-var circleX;
-var radius=25;
+var xPos;
 
+createCanvas(500, 200);
 function setup(){
-    createCanvas(800, 600);
-
-    circleX=0;
-    fill(255, 0, 0);
-}
-
-function draw(){
-    background(200);
-    circle(circleX, height/2, radius*2);
-    circleX=(circleX+5)%width
+    fill(255);
+xPos=0
 }
