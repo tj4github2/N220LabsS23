1068eb2f0677a6bee4f1b099f5030262577114f0
tj4github2 <tjmac1@icloud.com>
2023-01-26 16:20:31 -0500

    added js folder to lab4

---
+++ labs/lab4/js/app.js
@@ -0,0 +1,25 @@
+let x = 320;
+let y = 180;
+let xspeed = 5;
+let yspeed = 2;
+
+let r = 25;
+
+function setup() {
+  createCanvas(640, 360);
+}
+
+function draw() {
+  background(0);
+  ellipse(x, y, r*2, r*2);
+  x += xspeed;
+  y += yspeed;
+  if (x > width - r || x < r) {
+    xspeed = -xspeed;
+  }
+  if (y > height - r || y < r) {
+    yspeed = -yspeed;
+  }
+
+
+}
